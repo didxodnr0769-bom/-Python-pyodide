d3c4a6f75a45cfb07fb8f50b972d8c3fe2a10498
yang <didxodnr0769@gmail.com>
2024-03-02 20:37:17 +0900

    Feat : 반복문을 파이썬에서 실행하여 데이터 생성 속도 개선

--- js/pyodide.js
+++ js/pyodide.js
@@ -9,53 +9,28 @@ async function initPyodide() {
 }
 
 async function createData(checkedSet, count) {
-  let result = [];
   const checkedList = Array.from(checkedSet);
 
   let pythonExecuteCode = `
   from faker import Faker
   fake = Faker('ko_KR')
-  profile = {}
+  listObj = []
+  for _ in range(${count}):
+      profile = {}
   `;
-
   // 선택한 프로퍼티 리스트를 돌면서 faker로 데이터를 생성
   for (let i = 0; i < checkedList.length; i++) {
     pythonExecuteCode += `
-  profile["${checkedList[i]}"] = fake.${checkedList[i]}()
+      profile["${checkedList[i]}"] = fake.${checkedList[i]}()
   `;
   }
-
   pythonExecuteCode += `
-  profile
-  `;
+      listObj.append(profile)
+  listObj
+`;
 
-  for (let i = 0; i < count; i++) {
-    let pythonData = await pyodide.runPythonAsync(pythonExecuteCode);
+  const pythonResult = await pyodide.runPythonAsync(pythonExecuteCode);
+  const jsResult = pythonResult.toJs().map((item) => Object.fromEntries(item));
 
-    const profile = pythonData.toJs();
-    result.push(Object.fromEntries(profile));
-  }
-  return result;
+  return jsResult;
 }
-// let pythonData = await pyodide.runPythonAsync(`
-// from faker import Faker
-// fake = Faker('ko_KR')
-// profile = {}
-// profile["id"] = ${1}
-// profile["name"] = ${testName}
-// profile["address"] = fake.address()
-// profile["job"] = fake.job()
-// profile["email"] = fake.email()
-// profile["ssn"] = fake.ssn()
-// profile["phone_number"] = fake.phone_number()
-// profile["company"] = fake.company()
-// profile["url"] = fake.url()
-// profile["ipv4"] = fake.ipv4()
-// profile["ipv6"] = fake.ipv6()
-// profile["user_agent"] = fake.user_agent()
-// profile["hex_color"] = fake.hex_color()
-// profile["rgb_color"] = fake.rgb_color()
-// profile["safe_hex_color"] = fake.safe_hex_color()
-// profile["safe_color_name"] = fake.safe_color_name()
-// profile
-// `);

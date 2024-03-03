b38aa84d16caba9a7ef6499fe4bcbef6c8f8b4a2
yang <didxodnr0769@gmail.com>
2024-03-04 01:31:02 +0900

    Feat : 복사 기능 추가, 데이터 형식 추가 및 변경

--- fakerTest.py
+++ fakerTest.py
@@ -1,26 +1,55 @@
 from faker import Faker
+import random
 
 # Faker 객체 생성, 기본적으로 영어로 설정됩니다.
 fake = Faker('ko_KR')
-# 한국어 이름
-profile = {}
-profile["name"] = fake.name()
-profile["address"] = fake.address()
-profile["job"] = fake.job()
-profile["email"] = fake.email()
-profile["ssn"] = fake.ssn()
-profile["phone_number"] = fake.phone_number()
-profile["company"] = fake.company()
-profile["url"] = fake.url()
-profile["ipv4"] = fake.ipv4()
-profile["ipv6"] = fake.ipv6()
-profile["user_agent"] = fake.user_agent()
-profile["hex_color"] = fake.hex_color()
-profile["rgb_color"] = fake.rgb_color()
-profile["safe_hex_color"] = fake.safe_hex_color()
-profile["safe_color_name"] = fake.safe_color_name()
 
-return profile
+listObj = []
+
+for _ in range(1):
+    profile = {}
+    profile["name"] = fake.name()
+    profile["date"] = fake.date('%Y.%m.%d')
+    profile["bothify"] = fake.bothify(text='Product Number: ######-########')
+    # profile["address"] = fake.address()
+    # profile["job"] = fake.job()
+    # profile["email"] = fake.email()
+    # profile["ssn"] = fake.ssn()
+    # profile["phone_number"] = fake.phone_number()
+    # profile["company"] = fake.company()
+    # profile["url"] = fake.url()
+    # profile["ipv4"] = fake.ipv4()
+    # profile["ipv6"] = fake.ipv6()
+    # profile["user_agent"] = fake.user_agent()
+    # profile["hex_color"] = fake.hex_color()
+    # profile["rgb_color"] = fake.rgb_color()
+    # profile["safe_hex_color"] = fake.safe_hex_color()
+    # profile["safe_color_name"] = fake.safe_color_name()
+    listObj.append(profile)
+print(listObj)
+
+numb = '010-'+str(random.randint(1, 9999)).zfill(4)+'-'+str(random.randint(1, 9999)).zfill(4)
+print(numb)
+
+# 한국어 이름
+# profile = {}
+# profile["name"] = fake.name()
+# profile["address"] = fake.address()
+# profile["job"] = fake.job()
+# profile["email"] = fake.email()
+# profile["ssn"] = fake.ssn()
+# profile["phone_number"] = fake.phone_number()
+# profile["company"] = fake.company()
+# profile["url"] = fake.url()
+# profile["ipv4"] = fake.ipv4()
+# profile["ipv6"] = fake.ipv6()
+# profile["user_agent"] = fake.user_agent()
+# profile["hex_color"] = fake.hex_color()
+# profile["rgb_color"] = fake.rgb_color()
+# profile["safe_hex_color"] = fake.safe_hex_color()
+# profile["safe_color_name"] = fake.safe_color_name()
+
+# return profile
 
 # print(fake.profile())
 
--- js/pyodide.js
+++ js/pyodide.js
@@ -13,6 +13,7 @@ async function createData(checkedSet, count) {
 
   let pythonExecuteCode = `
   from faker import Faker
+  import random
   fake = Faker('ko_KR')
   listObj = []
   for _ in range(${count}):
@@ -20,17 +21,43 @@ async function createData(checkedSet, count) {
   `;
   // 선택한 프로퍼티 리스트를 돌면서 faker로 데이터를 생성
   for (let i = 0; i < checkedList.length; i++) {
-    pythonExecuteCode += `
-      profile["${checkedList[i]}"] = fake.${checkedList[i]}()
+    const property = checkedList[i];
+
+    // 커스텀하여 데이터를 생성할 경우 if문으로 처리
+    // date는 기본 yyyy-mm-dd 형식으로 생성되므로 다른 형식으로 생성하도록 처리
+    if (property === "date") {
+      pythonExecuteCode += `
+      profile["${property}"] = fake.${property}('%Y.%m.%d')
   `;
+    }
+    // phone number는 패턴이 적용되지 않아 직접 생성
+    else if (property === "phone_number") {
+      pythonExecuteCode += `
+      profile["${property}"] = '010-'+str(random.randint(1, 9999)).zfill(4)+'-'+str(random.randint(1, 9999)).zfill(4)
+  `;
+    }
+    // default
+    else {
+      pythonExecuteCode += `
+      profile["${property}"] = fake.${property}()
+  `;
+    }
   }
   pythonExecuteCode += `
       listObj.append(profile)
   listObj
 `;
+  console.log("실행 파이썬 코드 : ", pythonExecuteCode);
 
   const pythonResult = await pyodide.runPythonAsync(pythonExecuteCode);
   const jsResult = pythonResult.toJs().map((item) => Object.fromEntries(item));
 
-  return jsResult;
+  return jsResult.map((item, idx) => {
+    // 직접 JS에서 생성할 데이터 추가시
+    return {
+      index: idx,
+      id: Math.random().toString(36).substr(2, 9),
+      ...item,
+    };
+  });
 }
